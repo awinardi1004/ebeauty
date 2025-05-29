5f890a01a113b9ea88a30b1644f2d38cb5fb8545
awinardi1004 <awinardi1004@gmail.com>
2025-05-29 22:24:44 +0700

    fixed migration product_variants

--- database/migrations/2025_05_27_151752_create_product_variants_table.php
+++ database/migrations/2025_05_27_151752_create_product_variants_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('productvariants', function (Blueprint $table) {
+        Schema::create('product_variants', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->string('variant');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('productvariants');
+        Schema::dropIfExists('product_variants');
     }
 };
